64290e945a763ab79bc344d4e8375ce04a238d0e
tenyears <comexie@yahoo.com>
2018-09-17 23:44:58 +0800

    Init project

---
+++ ThreadSyncDemo/TSLockMgr.h
@@ -0,0 +1,13 @@
+//
+//  TSLockMgr.h
+//  ThreadSyncDemo
+//
+//  Created by UncleDrew on 2018/9/16.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSLockMgr : NSObject
+
+@end
---
+++ ThreadSyncDemo/TSLockMgr.m
@@ -0,0 +1,15 @@
+//
+//  TSLockMgr.m
+//  ThreadSyncDemo
+//
+//  Created by UncleDrew on 2018/9/16.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import "TSLockMgr.h"
+
+@implementation TSLockMgr
+
+
+
+@end
